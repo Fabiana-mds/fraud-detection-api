8dcd7e59e99ee8ad50fd3faf297cce2dbc333826
Fabiana-mds <byahbh@gmail.com>
2026-03-21 12:00:46 -0300

    feat: initial project structure with spring boot and dependencies

---
+++ src/main/java/com/projetofmds/fraudchecker/FraudcheckerApplication.java
@@ -0,0 +1,13 @@
+package com.projetofmds.fraudchecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FraudcheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FraudcheckerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projetofmds/fraudchecker/FraudcheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetofmds.fraudchecker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FraudcheckerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
